e07a361eb2fb64e3c265cf67cbc00c7fc5ea11db
n4tn <109428583+n4tn@users.noreply.github.com>
2022-10-06 17:55:53 +0100

    SelectionSort

--- SortManager/SortManagerApp/SelectionSort.cs
+++ SortManager/SortManagerApp/SelectionSort.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace SortManagerApp
 {
-    internal class SelectionSort
+    public class SelectionSort: ISortable
     {
+        public int[] Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                var min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if(array[min] > array[j])
+                    {
+                        min = j;
+                    }
+                }
+                if(min != i)
+                {
+                    int newMin = array[min];
+                    array[min] = array[i];
+                    array[i] = newMin;
+                }
+
+            }
+            return array;
+
+        }
     }
 }
