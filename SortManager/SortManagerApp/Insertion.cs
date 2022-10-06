e8c6a2246ee2007ee9744d70f27b1266ced8e35d
unknown <tudorbirgau@gmail.com>
2022-10-06 17:30:10 +0100

    Insertion sort

---
+++ SortManager/SortManagerApp/Insertion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortManagerApp
+{
+    internal class Insertion : ISortable
+    {
+        public int[] Sort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        int temp = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
