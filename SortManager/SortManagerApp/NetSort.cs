0b3355afe0258ba5623fcefe601e719b3eb4bee0
Lewis Kellett <lewisk1996@gmail.com>
2022-10-06 20:19:09 +0100

    Implmented .NET sort

---
+++ SortManager/SortManagerApp/NetSort.cs
@@ -0,0 +1,14 @@
+﻿namespace SortManagerApp
+{
+    public class NetSort : ISortable
+    {
+        public int[] Sort(int[] array)
+        {
+            if (array is null)
+                throw new ArgumentNullException();
+
+            Array.Sort(array);
+            return array;
+        }
+    }
+}
