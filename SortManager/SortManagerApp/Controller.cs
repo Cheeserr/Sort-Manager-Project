55bf69214acbdd7aa2ce8117385b3226132c0047
Sergiusz Pietrala <pietralasergiusz@gmail.com>
2022-10-06 17:46:49 +0100

    Started working on controller, basic structure for methods

--- SortManager/SortManagerApp/Controller.cs
+++ SortManager/SortManagerApp/Controller.cs
@@ -1,10 +1,26 @@
-﻿namespace SortManagerApp
+﻿using System.Drawing;
+
+namespace SortManagerApp
 {
     internal class Controller
     {
-        static void Main(string[] args)
+        void ChooseSort(int value, int sizeOfArray)
         {
-            Console.WriteLine("Hello, World!");
+            int[] array = GenerateArray(sizeOfArray);
+            switch (value)
+            {
+                case 0: array = BubbleSort.Sort(array);
+                    break;
+                case 1: array = MergeSort.Sort(array);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        int[] GenerateArray(int arraySize)
+        {
+            return new int[arraySize];
         }
     }
 }
