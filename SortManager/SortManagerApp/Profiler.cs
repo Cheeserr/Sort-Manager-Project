09f6d98d8e6161fb1b9391a5bb58fd3257f14fa9
Lewis Kellett <lewisk1996@gmail.com>
2022-10-06 17:26:39 +0100

    Started Profiler.cs

---
+++ SortManager/SortManagerApp/Profiler.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace SortManagerApp
+{
+    public class Profiler
+    {
+        Stopwatch _stopwatch;
+        public double Milliseconds
+        {
+            get
+            {
+                return _stopwatch.Elapsed.TotalMilliseconds;
+            }
+        }
+
+        public Profiler()
+        {
+            _stopwatch = new Stopwatch();
+        }
+
+        public T ProfileFunction<T>(Func<T> func)
+        {
+            _stopwatch.Reset();
+            _stopwatch.Start();
+            var result = func.Invoke();
+            _stopwatch.Stop();
+            return result;
+        }
+
+
+    }
+}
