484ba2148ca5192b21c19638ab0b0ae81816e40a
Sergiusz Pietrala <pietralasergiusz@gmail.com>
2022-10-07 09:59:50 +0100

    Added working generate array method and changed structure of controller

--- SortManager/SortManagerApp/Controller.cs
+++ SortManager/SortManagerApp/Controller.cs
@@ -2,16 +2,22 @@
 
 namespace SortManagerApp
 {
-    internal class Controller
+    public class Controller
     {
+        ISortable? sortMethod = null;
+        int[] array;
+
         void ChooseSort(int value, int sizeOfArray)
         {
-            int[] array = GenerateArray(sizeOfArray);
             switch (value)
             {
-                case 0: array = BubbleSort.Sort(array);
+                case 0: sortMethod = new BubbleSort();
                     break;
-                case 1: array = MergeSort.Sort(array);
+                case 1:
+                    sortMethod = new MergeSort();
+                    break;
+                case 2:
+                    sortMethod = new SelectionSort();
                     break;
                 default:
                     break;
@@ -20,7 +26,15 @@ namespace SortManagerApp
 
         int[] GenerateArray(int arraySize)
         {
-            return new int[arraySize];
+            Random random = new Random();
+            int[] output = new int[arraySize];
+
+            for(int i = 0; i < arraySize; i++)
+            {
+                output[i] = random.Next(1, 100);
+            }
+
+            return output;
         }
     }
 }
