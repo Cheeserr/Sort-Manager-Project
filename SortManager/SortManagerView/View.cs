4cfeafa4259c0488a292d99ed6f303cccfd51034
Sergiusz Pietrala <pietralasergiusz@gmail.com>
2022-10-07 10:34:46 +0100

    Continue working on project structure

--- SortManager/SortManagerView/View.cs
+++ SortManager/SortManagerView/View.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SortManagerController;
 
-namespace SortManagerApp
+namespace SortManagerView
 {
     internal class View
     {
+
     }
 }
