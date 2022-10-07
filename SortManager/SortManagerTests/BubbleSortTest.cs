da1ab6912e079192e743e54342fe649ff6b8709a
Lewis Kellett <lewisk1996@gmail.com>
2022-10-07 10:55:34 +0100

    Fixed solution, It builds!

--- SortManager/SortManagerTests/BubbleSortTest.cs
+++ SortManager/SortManagerTests/BubbleSortTest.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
-using SortManagerApp;
-using SortManagerModel;
+﻿using SortManagerModel;
 
 namespace SortManagerTests;
 
--- SortManager/SortManagerView/View.cs
+++ SortManager/SortManagerView/View.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SortManagerController;
-
-namespace SortManagerView
+﻿namespace SortManagerView
 {
     internal class View
     {
+        public static void Main()
+        {
 
+        }
     }
 }
