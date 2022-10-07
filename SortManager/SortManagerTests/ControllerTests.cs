c411f1deb86e1c6b18be14988f73a3a19d3aed6c
Ad Mill <admillard01@gmail.com>
2022-10-07 16:12:35 +0100

    calc

--- SortManager/SortManagerTests/ControllerTests.cs
+++ SortManager/SortManagerTests/ControllerTests.cs
@@ -1,18 +1,17 @@
-namespace SortManagerTests
+using SortManagerController;
+
+namespace SortManagerTests;
+
+public class Tests
 {
-    public class Tests
+
+    [TestCase(0)]
+    [TestCase(1)]
+    [TestCase(10)]
+    public void GivenArrayLength_teurnsArrayOfCorrectLerngth(int length)
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void Test1()
-        {
-            Random rand = new Random((int)1);
-
-            Assert.Pass();
-        }
+        //Random rand = new Random((int)1);
+        int[] result = Controller.ArrayGenerator(length);
+        Assert.That(result.Length,Is.EqualTo(length));
     }
 }
