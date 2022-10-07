7773f68564bd4c3df598e74b4289e115119d6edd
Sergiusz Pietrala <pietralasergiusz@gmail.com>
2022-10-07 11:56:37 +0100

    Cleaning Main

--- SortManager/SortManagerView/View.cs
+++ SortManager/SortManagerView/View.cs
@@ -6,14 +6,6 @@ namespace SortManagerView
     {
         public static void Main()
         {
-            Controller controller = new Controller(10);
-
-            int[] array;
-            controller.ChooseSort(1);
-            array = controller.SortArray();
-
-            Console.WriteLine(controller.ArrayToString());
-            Console.WriteLine($"{controller.profiler.Milliseconds} milliseconds");
 
         }
     }
