30500b54b3e36c0bd036f0561bb81771ae229c05
Ad Mill <admillard01@gmail.com>
2022-10-07 12:04:42 +0100

    heapsort tests

---
+++ SortManager/SortManagerTests/HeapSortTests.cs
@@ -0,0 +1,33 @@
+﻿using SortManagerModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortManagerTests;
+
+internal class HeapSortTests
+{
+    [TestCaseSource(typeof(SortTestData), nameof(SortTestData.PosArrays))]
+    public void GivenAnArrayof1Integers_ReturnsMergeSortedArray(int[] input, int[] expected)
+    {
+        var sorter = new HeapSort();
+        var actual = sorter.Sort(input);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+    [TestCaseSource(typeof(SortTestData), nameof(SortTestData.EmptArray))]
+    public void GivenAnEmptyArray_returnsAnEmptyArray(int[] input, int[] expected)
+    {
+        var sorter = new HeapSort();
+        var actual = sorter.Sort(input);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+    [TestCaseSource(typeof(SortTestData), nameof(SortTestData.NegandPosArrays))]
+    public void GivenAnArrayofMixedIntegers_ReturnsMergeSortedArray(int[] input, int[] expected)
+    {
+        var sorter = new HeapSort();
+        var actual = sorter.Sort(input);
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
